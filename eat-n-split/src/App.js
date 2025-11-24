d32817ceed5574c63e3a32ab2d0a421998265378
kelechi-aims <kelechi.denise@yahoo.com>
2025-11-24 07:18:14 +0100

    completed eat-n-split

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -40,6 +40,20 @@ export default function App() {
     setSelectedFriend((cur) => (cur?.id === friend.id ? null : friend));
     setShowAddFriend(false);
   }
+
+  function handleSplitBill(value) {
+    console.log(value);
+
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
+    setSelectedFriend(null);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
@@ -56,7 +70,12 @@ export default function App() {
         </Button>
       </div>
 
-      {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} />}
+      {selectedFriend && (
+        <FormSplitBill
+          selectedFriend={selectedFriend}
+          onSpiltBill={handleSplitBill}
+        />
+      )}
     </div>
   );
 }
@@ -154,14 +173,21 @@ function FormAddFriend({ onAddFriend }) {
   );
 }
 
-function FormSplitBill({ selectedFriend }) {
+function FormSplitBill({ selectedFriend, onSpiltBill }) {
   const [bill, setBill] = useState("");
   const [yourExpense, setYourExpense] = useState("");
   const friendExpense = bill ? bill - yourExpense : "";
   const [payer, setPayer] = useState("you");
 
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!bill || !yourExpense) return;
+    onSpiltBill(payer === "you" ? friendExpense : -yourExpense);
+  }
+
   return (
-    <form className="form-split-bill">
+    <form className="form-split-bill" onSubmit={handleSubmit}>
       <h2>Split a bill with {selectedFriend.name}</h2>
 
       <label>Bill value</label>
